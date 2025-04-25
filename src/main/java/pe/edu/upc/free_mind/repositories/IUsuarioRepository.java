ed7c09d3a25626f0fe51d822aa74c2f2cb56c8c3
RenzoDPV <149837338+RenzoDPV@users.noreply.github.com>
2025-04-25 16:34:46 -0500

    Update IUsuarioRepository.java

--- src/main/java/pe/edu/upc/free_mind/repositories/IUsuarioRepository.java
+++ src/main/java/pe/edu/upc/free_mind/repositories/IUsuarioRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import pe.edu.upc.free_mind.entities.Usuario;
 
 /**
- * Repositorio JPA para la entidad Usuario.
  * Permite operaciones CRUD básicas sin necesidad de implementación explícita.
  */
 @Repository
@@ -13,6 +12,22 @@ public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
     // Aquí se pueden definir consultas personalizadas si se requiere
 
     Usuario findByCorreo(String correo);
+
+    /* Monto total pagado de los clientes */
+    @Query(value = "SELECT u.nombre, SUM(p.monto)\n" +
+            " FROM Usuario u\n" +
+            " INNER JOIN Pago p\n" +
+            " on u.id_usuario = p.id_usuario\n" +
+            " GROUP BY u.nombre\n", nativeQuery = true)
+    public List<String[]> amountByUsuario();
+
+    /*Cantidad de comentarios de los usuarios */
+    @Query(value = "SELECT u.nombre, COUNT(c.id_comentario)\n" +
+            " FROM Usuario u\n" +
+            " INNER JOIN Comentario c\n" +
+            " on u.id_usuario = c.id_usuario\n" +
+            " GROUP BY u.nombre", nativeQuery = true)
+    public List<String[]> comentByUsuario();
 }
 
 
