8d0f23b8657dcd4806d3d2d8232d9cfbb49e872c
Deyci H <208078952+Deyci22@users.noreply.github.com>
2025-06-30 19:19:29 -0500

    update cita

--- src/main/java/pe/edu/upc/free_mind/controllers/CitaController.java
+++ src/main/java/pe/edu/upc/free_mind/controllers/CitaController.java
@@ -1,8 +1,9 @@
 package pe.edu.upc.free_mind.controllers;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.free_mind.dtos.CantidadCitasPorTerapiaDTO;
+import pe.edu.upc.free_mind.dtos.CantidadTotalIngresosPorPsicologoDTO;
 import pe.edu.upc.free_mind.dtos.CitaDTO;
 import pe.edu.upc.free_mind.entities.Cita;
 import pe.edu.upc.free_mind.entities.Horario;
@@ -13,6 +14,7 @@ import pe.edu.upc.free_mind.servicesinterfaces.IHorarioService;
 import pe.edu.upc.free_mind.servicesinterfaces.IUsuarioService;
 import pe.edu.upc.free_mind.servicesinterfaces.ITerapiaService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -116,4 +118,34 @@ public class CitaController {
     public void eliminar(@PathVariable("id") Integer id) {
         citaService.delete(id);
     }
+
+    //Reportes - Deyci
+    //Obtiene la cantidad de citas por tipo de terapias
+    @GetMapping("/cantidadCitasPorTerapia")
+    public List<CantidadCitasPorTerapiaDTO> obtenerCantidadCitasPorTerapia() {
+        List<CantidadCitasPorTerapiaDTO> dtoLista = new ArrayList<>();
+        List<String[]> fila = citaService.obtenerCantidadCitasPorTerapia();
+        for (String[] columna : fila) {
+            CantidadCitasPorTerapiaDTO dto = new CantidadCitasPorTerapiaDTO();
+            dto.setNameTerapia(columna[0]);
+            dto.setDescripcion(columna[1]);
+            dto.setQuantityCitas(Integer.parseInt(columna[2]));
+            dtoLista.add(dto);
+        }
+        return dtoLista;
+    }
+    //Obtiene el total de ingresos por psicólogo
+    @GetMapping("/totalIngresosPsicologos")
+    public List<CantidadTotalIngresosPorPsicologoDTO> obtenerTotalIngresosPorPsicologo() {
+        List<CantidadTotalIngresosPorPsicologoDTO> dtoLista = new ArrayList<>();
+        List<String[]> fila = citaService.totalIngresosPorPsicologo();
+        for (String[] columna : fila) {
+            CantidadTotalIngresosPorPsicologoDTO dto = new CantidadTotalIngresosPorPsicologoDTO();
+            dto.setNombre(columna[0]);
+            dto.setApellido(columna[1]);
+            dto.setTotalIngresos(Integer.parseInt(columna[2]));
+            dtoLista.add(dto);
+        }
+            return dtoLista;
+        }
 }
